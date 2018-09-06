8bffd960ddc08cf092ca36e1b16eaa886d8a4cc5
User <User@DESKTOP-EV21BNK>
2018-09-06 18:03:00 +0300

    Add project files.

---
+++ DXApplication2/LoginBox.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace DXApplication2
+{
+    partial class LoginBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginEdit = new DevExpress.XtraEditors.TextEdit();
+            this.PassEdit = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PassEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoginEdit
+            // 
+            this.LoginEdit.Location = new System.Drawing.Point(122, 80);
+            this.LoginEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LoginEdit.Name = "LoginEdit";
+            this.LoginEdit.Properties.Appearance.Font = new System.Drawing.Font("DINPro-Regular", 11.25F);
+            this.LoginEdit.Properties.Appearance.Options.UseFont = true;
+            this.LoginEdit.Size = new System.Drawing.Size(227, 26);
+            this.LoginEdit.TabIndex = 0;
+            // 
+            // PassEdit
+            // 
+            this.PassEdit.Location = new System.Drawing.Point(122, 118);
+            this.PassEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PassEdit.Name = "PassEdit";
+            this.PassEdit.Properties.Appearance.Font = new System.Drawing.Font("DINPro-Regular", 11.25F);
+            this.PassEdit.Properties.Appearance.Options.UseFont = true;
+            this.PassEdit.Properties.PasswordChar = '*';
+            this.PassEdit.Size = new System.Drawing.Size(227, 26);
+            this.PassEdit.TabIndex = 1;
+            this.PassEdit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PassEdit_KeyPress);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("DINPro-Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(79, 82);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(35, 20);
+            this.labelControl1.TabIndex = 2;
+            this.labelControl1.Text = "User";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("DINPro-Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(42, 124);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(72, 20);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "Password";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(236, 156);
+            this.simpleButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(113, 34);
+            this.simpleButton1.TabIndex = 4;
+            this.simpleButton1.Text = "Login";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            this.simpleButton1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LoginBox_KeyPress);
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureEdit1.EditValue = global::DXApplication2.Properties.Resources._2boton_inactivo;
+            this.pictureEdit1.Location = new System.Drawing.Point(0, 1);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.StretchHorizontal;
+            this.pictureEdit1.Properties.ZoomAccelerationFactor = 1D;
+            this.pictureEdit1.Properties.ZoomPercent = 500D;
+            this.pictureEdit1.Size = new System.Drawing.Size(385, 47);
+            this.pictureEdit1.TabIndex = 5;
+            // 
+            // LoginBox
+            // 
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 203);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.PassEdit);
+            this.Controls.Add(this.LoginEdit);
+            this.Font = new System.Drawing.Font("DINPro-Regular", 11.25F);
+            this.LookAndFeel.SkinName = "Liquid Sky";
+            this.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "LoginBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Login";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LoginBox_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.LoginEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PassEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit LoginEdit;
+        private DevExpress.XtraEditors.TextEdit PassEdit;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+    }
+}
---
+++ DXApplication2/LoginBox.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using DataHandler;
+using System.Security.Cryptography;
+using System.Collections;
+
+
+namespace DXApplication2
+{
+    public partial class LoginBox : DevExpress.XtraEditors.XtraForm
+    {
+       UserOptions Userop = new UserOptions();
+        Users user = new Users();
+        bool pass;
+        public LoginBox()
+        {
+          //  Userlist.AddRange(inUserList);
+            InitializeComponent();
+           
+        }
+
+        public bool Pass
+        {
+            get
+            {
+                return pass;
+            }
+
+        }
+
+        public Users Userlogged
+        {
+            get
+            {
+                return user;
+            }
+
+        }
+
+        private void simpleButton1_Click(object sender, System.EventArgs e)
+        {
+
+            user = Userop.GetUserby_Login(LoginEdit.Text);
+            if (user.Name != null)
+            {
+
+                byte[] passto = new byte[50];
+
+                byte[] password = Userop.GenerateHash(PassEdit.Text, PassEdit.Text.Length);
+                password.CopyTo(passto, 0);
+
+
+
+
+                IStructuralEquatable se1 = user.Password;
+
+
+
+                if (se1.Equals(passto, StructuralComparisons.StructuralEqualityComparer))
+                { pass = true; this.Close(); }
+
+            }
+            else
+            {
+
+            }
+
+
+
+
+
+
+        }
+
+        private void LoginBox_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void PassEdit_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r') simpleButton1.PerformClick();
+
+               
+        }
+    }
+}
